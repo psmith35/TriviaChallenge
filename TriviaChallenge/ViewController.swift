55009b8c75e4947b06e0f87c262f17728a681cde
tallpaul3486 <tallpaul3486@users.noreply.github.com>
2021-08-07 19:57:59 -0400

    Initial Commit

---
+++ TriviaChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaChallenge
+//
+//  Created by Paul Smith on 8/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
